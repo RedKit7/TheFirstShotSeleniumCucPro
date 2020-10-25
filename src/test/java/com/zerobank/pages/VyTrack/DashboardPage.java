6f2ba95a97b48aa68d448cd62c710a87de4f4df3
Ercan Cotenoglu <ercancotenoglu@gmail.com>
2020-10-25 11:42:51 +0000

    Initial commit, nextBase and zerobank added

---
+++ src/test/java/com/zerobank/pages/VyTrack/DashboardPage.java
@@ -0,0 +1,4 @@
+package com.zerobank.pages.VyTrack;
+
+public class DashboardPage extends BasePage {
+}
---
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -0,0 +1,25 @@
+package com.zerobank.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports"
+        },
+        features = "src/test/resources/features",
+        glue = "com/vytrack/step_definitions",
+        dryRun = false,
+        tags = "@wip",
+        strict = true
+        //  tags = "@driver or @VYT-123"
+        //  tags = "@driver and not @VYT-123"
+
+)
+public class CukesRunner {
+
+
+}
